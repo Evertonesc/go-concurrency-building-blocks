316002a4ed07805094bcd7f6837733490e665936
esc <evertonsouzaesc@gmail.com>
2023-11-18 22:14:30 -0300

    remove functions calls

--- main.go
+++ main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"draft/primtvs"
-)
-
 func main() {
-	resultStream := primtvs.ChanOwner()
-	for result := range resultStream {
-		fmt.Printf("Received: %d\n", result)
-	}
-
-	fmt.Println("Done receiving!")
-
-	primtvs.SimpleSelectStatement()
-
-	primtvs.EnqueueWithCond()
+	// Call functions here to see their behavior
 }
